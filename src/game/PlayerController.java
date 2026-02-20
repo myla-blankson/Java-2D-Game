3c5506b79e23c9730d347621148709b1ad5fb72f
myla-blankson <Myla.Blankson@city.ac.uk>
2026-02-20 18:06:59 +0000

    added player movement and beginning to create Coin class

---
+++ src/game/PlayerController.java
@@ -0,0 +1,55 @@
+package game;
+
+import city.cs.engine.Body;
+import city.cs.engine.BodyImage;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.HashMap;
+
+public class PlayerController implements KeyListener { // remember to register event...
+// all methods must be implemented and overridden or class must be declared as abstract...
+    private static final float speed = 2;
+    private Player player;
+
+    public PlayerController(Player player) {
+        this.player = player;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_LEFT) {
+            player.startWalking(-speed); // implementing method from Walker class and applying to Player instance, 'player'
+        } else if (code == KeyEvent.VK_RIGHT) {
+            player.startWalking(speed * 2);
+        } else if (code == KeyEvent.VK_UP) {
+            player.jump(5);
+        }else if (code == KeyEvent.VK_DOWN) {
+            player.jump(-5);
+        }
+
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_LEFT) {
+            player.stopWalking(); // implementing
+        } else if (code == KeyEvent.VK_RIGHT) {
+            player.stopWalking();
+
+    }
+
+}
+
+
+
+
+}
