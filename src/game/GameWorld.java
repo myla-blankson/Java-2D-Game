a8c5aa8d074d823780aa373a972ea5d561d352e8
myla-blankson <Myla.Blankson@city.ac.uk>
2026-03-06 12:21:38 +0000

    key events finally working

--- src/game/GameWorld.java
+++ src/game/GameWorld.java
@@ -7,13 +7,13 @@ import city.cs.engine.Body;
 
 
 
-public class GameWorld extends World{
+public class GameWorld extends World {
 
     private Player player;
     private boolean gameWon = false;
-   private boolean  gameOver = false;
-   private int timeRemaining = 60;
-   private Timer timer;
+    private boolean gameOver = false;
+    private int timeRemaining = 60;
+    private Timer timer;
 
 
     public GameWorld() {
@@ -36,7 +36,6 @@ public class GameWorld extends World{
         platform2.addImage(new BodyImage("./data/platform.png", 1f));
 
 
-
         player = new Player(this);
         player.setPosition(new Vec2(8, -10));
         PlayerCollisions pickup = new PlayerCollisions(player);
@@ -55,10 +54,48 @@ public class GameWorld extends World{
         coin2.setPosition(new Vec2(10, 3));
         coin2.addCollisionListener(new PlayerCollisions(player));
 
+        // Coin 3
+        Coin coin3 = new Coin(this);
+        coin3.setPosition(new Vec2(15, -2));
+        coin3.addCollisionListener(new PlayerCollisions(player));
+
+        // Coin 4
+        Coin coin4 = new Coin(this);
+        coin4.setPosition(new Vec2(-6, 3));
+        coin4.addCollisionListener(new PlayerCollisions(player));
+
+        // Coin 5
+        Coin coin5 = new Coin(this);
+        coin5.setPosition(new Vec2(-10, -7));
+        coin5.addCollisionListener(new PlayerCollisions(player));
+
+        // Coin 6
+        Coin coin6 = new Coin(this);
+        coin6.setPosition(new Vec2(0, 7));
+        coin6.addCollisionListener(new PlayerCollisions(player));
+
+        // Coin 7
+        Coin coin7 = new Coin(this);
+        coin7.setPosition(new Vec2(20, -6));
+        coin7.addCollisionListener(new PlayerCollisions(player));
+
+        // Coin 8
+        Coin coin8 = new Coin(this);
+        coin8.setPosition(new Vec2(18, 5));
+        coin8.addCollisionListener(new PlayerCollisions(player));
+
+        // Coin 9
+        Coin coin9 = new Coin(this);
+        coin9.setPosition(new Vec2(-15, -3));
+        coin9.addCollisionListener(new PlayerCollisions(player));
+
+        // Coin 10
+        Coin coin10 = new Coin(this);
+        coin10.setPosition(new Vec2(-12, 6));
+        coin10.addCollisionListener(new PlayerCollisions(player));
+
+
         // could create an array storing multiple coins in random areas...
-
-
-
 
 
     }
@@ -67,14 +104,20 @@ public class GameWorld extends World{
     public Player getPlayer() {
         return player;
     }
-    public int getTimeRemaining(){
+
+    public int getTimeRemaining() {
         return timeRemaining;
     }
+
     public boolean isGameOver() {
         return gameOver;
     }
-    public boolean isGameWon() {
-        return gameWon;
-    }
 
-}
+    public boolean isGameWon() {
+            return gameWon;
+        }
+
+
+
+
+    }
